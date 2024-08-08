c372ada65a00b4dd2db5b8b458723461feca8592
Automatic Encoder <156506190+AutomaticEncoder@users.noreply.github.com>
2024-08-08 14:04:44 +0300

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import webbrowser
 webbrowser.register('Chrome', None, webbrowser.BackgroundBrowser('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'))
-webbrowser.get('Chrome').open_new_tab('https://gist.github.com/idprogramming8/8d30520da3178df4ed7f24c15df12bbc')
+webbrowser.get('Chrome').open_new_tab('https://gist.github.com/AutomaticEncoder/8d30520da3178df4ed7f24c15df12bbc')
